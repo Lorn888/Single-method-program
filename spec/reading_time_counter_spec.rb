2175648303c6c668df683386a9c140238b8d09e2
Patryk <patrykokpara@hotmail.com>
2022-09-06 23:22:54 +0100

    Wrote a 2nd pasing test for reading_time_counter

--- spec/reading_time_counter_spec.rb
+++ spec/reading_time_counter_spec.rb
@@ -1,8 +1,14 @@
 require 'reading_time_counter'
 
 RSpec.describe "reading_time_counter " do
-    it "calculates time required to read text" do
+    it "empty rext returns 0" do
         result = reading_time_counter(" ")
         expect(result).to eq 0
     end
+    it "less than 200 words returns 1" do
+        result = reading_time_counter("some words" * 10)
+        expect(result).to eq 1
+    end
+    
+
 end
