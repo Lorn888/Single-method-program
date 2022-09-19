2c8565342769593513401513498272b269799e1a
Patryk <patrykokpara@hotmail.com>
2022-09-19 23:58:31 +0100

    Wrote and passed the 3rd test for does_include method

--- spec/does_include_spec.rb
+++ spec/does_include_spec.rb
@@ -9,4 +9,8 @@ RSpec.describe "does_include" do
         result = does_include("#TODO")
         expect(result).to eq true
     end
+    it "checks if text '#todo' returns false" do
+        result = does_include("#todo")
+        expect(result).to eq false
+    end
 end
